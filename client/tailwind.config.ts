82a7e0c6834004340c08e5071107d3a04570cb1c
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2024-10-10 20:10:53 -0600

    fill out tokyonight theme
    shout out to folke

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -6,15 +6,45 @@ export default {
 		extend: {
 			colors: {
 				tokyonight: {
-					bg: {
+					background: {
 						DEFAULT: "#222436",
 						dark: "#1e2030",
-						highlight: "#2f334d",
 					},
-					fg: {
+					foreground: {
 						DEFAULT: "#c8d3f5",
 						dark: "#828bb8",
-						gutter: "#3b4261",
+					},
+					black: {
+						DEFAULT: "#1b1d2b",
+						bright: "#444a73",
+					},
+					red: {
+						DEFAULT: "#ff757f",
+						bright: "#c53b53",
+					},
+					green: {
+						DEFAULT: "#c3e88d",
+						bright: "#4fd6be",
+					},
+					yellow: {
+						DEFAULT: "#ffc777",
+						bright: "#ff966c",
+					},
+					blue: {
+						DEFAULT: "#82aaff",
+						bright: "#65bcff",
+					},
+					magenta: {
+						DEFAULT: "#c099ff",
+						bright: "#ff007c",
+					},
+					cyan: {
+						DEFAULT: "#86e1fc",
+						bright: "#89ddff",
+					},
+					white: {
+						DEFAULT: "#828bb8",
+						bright: "#c8d3f5",
 					},
 				},
 			},
