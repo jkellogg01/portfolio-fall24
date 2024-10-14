9553794351e5091bf223ae147fde6bdcd457b24c
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2024-10-13 20:16:58 -0600

    some layout tinkery, i think i like it now

--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export function Card({ children }: { children: ReactNode }) {
 	return (
-		<div className="border border-tokyonight-foreground bg-tokyonight-background-dark p-4 rounded-lg">
+		<div className="border border-tokyonight-foreground bg-tokyonight-background-dark p-4 rounded-lg w-fit">
 			{children}
 		</div>
 	);
