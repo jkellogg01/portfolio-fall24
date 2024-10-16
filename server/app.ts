5aed6b21b6696b3ad7097021db74f94ddf73b0d2
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2024-10-16 16:43:34 -0600

    listen, okay?
    if I told you that I forgot to serve the static react files from my js
    server which mostly exists to serve the static react files would you
    judge me? or would you show me compassion.
    
    I thought I did that many many days ago so I just never looked at it.

--- server/app.ts
+++ server/app.ts
@@ -3,6 +3,7 @@ import { logger } from "hono/logger";
 import { spotifyRoute } from "./routes/spotify";
 import { callbackRoute } from "./routes/callback";
 import { contactRoute } from "./routes/contact";
+import { serveStatic } from "hono/bun";
 
 const app = new Hono();
 app.use(logger());
@@ -12,6 +13,8 @@ const api = app
   .route("/spotify", spotifyRoute)
   .route("/callback", callbackRoute)
   .route("/contact", contactRoute);
+
+app.use("*", serveStatic({ root: "./client/dist" }));
 
 export type AppType = typeof api;
 
