a5dd1b3d84f5a1edd4cf58916db47b9227698e3f
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2024-10-16 09:24:45 -0600

    I think I just fixed the error state but idk why I didn't commit

--- client/src/components/Card.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-
-export function Card({ children }: { children: ReactNode }) {
-	return (
-		<div className="border border-tokyonight-foreground bg-tokyonight-background-dark p-4 rounded-lg w-fit">
-			{children}
-		</div>
-	);
-}
