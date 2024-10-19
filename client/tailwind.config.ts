eacb866cfb316030b83f8fb7a5337eaa3782747c
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2024-10-18 21:46:46 -0600

    apply fonts
    probably could have written a script for this too but i tried to do it
    with vim macros instead and now I am in pain

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,54 +1,59 @@
 import type { Config } from "tailwindcss";
 
 export default {
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-	theme: {
-		extend: {
-			colors: {
-				tokyonight: {
-					background: {
-						DEFAULT: "#222436",
-						dark: "#1e2030",
-					},
-					foreground: {
-						DEFAULT: "#c8d3f5",
-						dark: "#828bb8",
-					},
-					black: {
-						DEFAULT: "#1b1d2b",
-						bright: "#444a73",
-					},
-					red: {
-						DEFAULT: "#ff757f",
-						bright: "#c53b53",
-					},
-					green: {
-						DEFAULT: "#c3e88d",
-						bright: "#4fd6be",
-					},
-					yellow: {
-						DEFAULT: "#ffc777",
-						bright: "#ff966c",
-					},
-					blue: {
-						DEFAULT: "#82aaff",
-						bright: "#65bcff",
-					},
-					magenta: {
-						DEFAULT: "#c099ff",
-						bright: "#ff007c",
-					},
-					cyan: {
-						DEFAULT: "#86e1fc",
-						bright: "#89ddff",
-					},
-					white: {
-						DEFAULT: "#828bb8",
-						bright: "#c8d3f5",
-					},
-				},
-			},
-		},
-	},
-	plugins: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      sans: "ibm plex sans",
+      serif: "ibm plex serif",
+      mono: "ibm plex mono",
+    },
+    extend: {
+      colors: {
+        tokyonight: {
+          background: {
+            DEFAULT: "#222436",
+            dark: "#1e2030",
+          },
+          foreground: {
+            DEFAULT: "#c8d3f5",
+            dark: "#828bb8",
+          },
+          black: {
+            DEFAULT: "#1b1d2b",
+            bright: "#444a73",
+          },
+          red: {
+            DEFAULT: "#ff757f",
+            bright: "#c53b53",
+          },
+          green: {
+            DEFAULT: "#c3e88d",
+            bright: "#4fd6be",
+          },
+          yellow: {
+            DEFAULT: "#ffc777",
+            bright: "#ff966c",
+          },
+          blue: {
+            DEFAULT: "#82aaff",
+            bright: "#65bcff",
+          },
+          magenta: {
+            DEFAULT: "#c099ff",
+            bright: "#ff007c",
+          },
+          cyan: {
+            DEFAULT: "#86e1fc",
+            bright: "#89ddff",
+          },
+          white: {
+            DEFAULT: "#828bb8",
+            bright: "#c8d3f5",
+          },
+        },
+      },
+    },
+  },
+  plugins: [],
 } satisfies Config;
